973ae4377dd2acc23a61bf1c0c9833a3dd398fc0
shahidafridi-swe <shahidafridi.cse@gmail.com>
2024-08-31 21:16:51 +0600

    allowed host

--- search_coders/settings.py
+++ search_coders/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # CORS_ALLOWED_ORIGINS = [
 #     "http://127.0.0.1:5500",
@@ -40,7 +40,7 @@ ALLOWED_HOSTS = []
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
-CSRF_TRUSTED_ORIGINS =[ 'http://127.0.0.1:8000/']
+CSRF_TRUSTED_ORIGINS =[ 'http://127.0.0.1:8000/', 'https://search-coders.onrender.com']
 
 
 # Application definition
